bbe7deded7814041d9667442b8259e309e5fd4ad
eduardo henrique <=>
2023-02-17 18:07:02 -0300

    feat: theme mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,20 @@
+import css from "./style.module.css";
+import { Header } from "../components/Header";
+import { useState } from "react";
+import { clsx } from "clsx";
 export function App() {
-  return <></>;
+  const [light, setLight] = useState(false);
+  console.log(light);
+
+  const classList = clsx({
+    [css.App]: true,
+    [css.light]: light,
+  });
+
+  return (
+    <div className={classList}>
+      <Header />
+      <button onClick={() => setLight(!light)}>theme</button>
+    </div>
+  );
 }
